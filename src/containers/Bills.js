9813df18600355e41c00a9a211949718a84cf54a
Stebenwolf <benjamin@the-org.eu>
2022-03-25 15:45:54 +0100

    Partial fix : antichrono date

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -27,20 +27,34 @@ export default class {
     $('#modaleFile').modal('show')
   }
 
+  sortByDate = (list) => {
+    const byDate = list.slice(0)
+    byDate.sort((a, b) => {
+        // pour trier les dates qui sont actuellement au format string "YYYY-MM-DD", on compare leur timestamp via un Date.parse(), sans changer le format de la variable.
+        return Date.parse(a.date) - Date.parse(b.date)
+    })
+    return byDate
+  }
+
   getBills = () => {
     if (this.store) {
       return this.store
       .bills()
       .list()
       .then(snapshot => {
-        const bills = snapshot
+        
+        const bills = this.sortByDate(snapshot)
           .map(doc => {
             try {
+              console.log(doc.date)
+              
               return {
                 ...doc,
-                date: formatDate(doc.date),
+                //date: formatDate(doc.date),
+                date: doc.date,
                 status: formatStatus(doc.status)
               }
+              
             } catch(e) {
               // if for some reason, corrupted data was introduced, we manage here failing formatDate function
               // log the error and return unformatted date in that case
